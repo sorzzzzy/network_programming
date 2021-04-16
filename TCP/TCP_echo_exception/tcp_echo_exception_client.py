6399508e21e98b677623f9eadcaf5dd6bdbe05cb
Jinsol Jeong <jinsol0330@naver.com>
2021-04-16 23:46:29 +0900

    TCP practice

--- TCP/TCP_echo_exception/tcp_echo_exception_client.py
+++ TCP/TCP_echo_exception/tcp_echo_exception_client.py
@@ -1,3 +1,12 @@
+# 예외처리
+# 상대방과의 연결이 끊어졌을 때 send()/recv()를 호출하면 에러 발생
+# 정상적인 프로그램 실행을 위해 예외 처리 필요
+
+# 서버가 비정상적으로 종료되는 경우,
+# 클라이언트의 send() 함수에서 connectionreseterror 발생 후 프로그램 종료
+# 클라이언트가 비정상적으로 종료되는 경우,
+# 서버의 recv() 함수에서 connectionreseterror 발생 후 프로그램 종료
+
 import socket
 
 port = int(input("Port No: "))
@@ -10,9 +19,8 @@ s.connect(address)
 while True:
     msg = input("Message to send: ")
     try:
-        # 인코딩해서 전송
         bytesSent = s.send(msg.encode())
-    except:
+    except:     # 송신할 때 예외 발생
         print('connection closed')
         break 
     else:
@@ -20,13 +28,12 @@ while True:
         print("{} bytes send".format(bytesSent))
 
     try:
-        # 데이터 받기
         data = s.recv(BUFSIZE)
-    except:
+    except:     # 수신할 때 예외 발생
         print('connection closed') 
         break
     else:
-        if not data:
+        if not data:    # 데이터 없는 경우
             break
         print("Received message: %s" %data.decode()) 
 s.close()
