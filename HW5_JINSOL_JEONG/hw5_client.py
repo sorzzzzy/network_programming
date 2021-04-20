0e07eb8c67fb0eb923c157aecd8a15c78f62a309
Jinsol Jeong <jinsol0330@naver.com>
2021-04-21 00:27:42 +0900

    HW5_add comment

--- HW5_JINSOL_JEONG/hw5_client.py
+++ HW5_JINSOL_JEONG/hw5_client.py
@@ -1,28 +1,43 @@
-import socket
+# TCP 계산기 클라이언트
+#   -> 사용자로부터 “20 + 17” 형태의 계산식을 입력 받음
+#   -> 지원 연산: 더하기, 빼기, 곱하기, 나누기(소수점 1자리까지 표시)
+#   -> 입력 받은 계산식을 서버로 전송
+#   -> 서버로부터 전송 받은 결과를 화면에 출력
+#   -> 사용자가 “q”를 입력하면 종료
+
+from socket import *
 
 address = ("localhost", 3333)
 BUFSIZE = 1024
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket(AF_INET, SOCK_STREAM)
 s.connect(address)
 
 while True:
     msg = input("Input : ")
+
+    # 'q'를 입력하면 종료
     if msg == 'q':  
         break
+
+    # 그게 아니라면,
     else:
+        # 계산식을 서버로 전송
         try:
-            bytesSent = s.send(msg.encode())   
+            bytesSent = s.send(msg.encode()) 
+        # 예외처리  
         except:
             print('connection closed')
             break
 
+        # 계산 결과를 수신
         try:
             data = s.recv(BUFSIZE)
         except:
             print('connection closed')
             break
         else:
+            # 데이터를 수신할 때 꼭 필요한 예외처리
             if not data:
                 break
             res = data.decode()
--- HW5_JINSOL_JEONG/hw5_server.py
+++ HW5_JINSOL_JEONG/hw5_server.py
@@ -1,3 +1,6 @@
+# TCP 계산기 서버
+#   -> 클라이언트로부터 계산식을 수신한 후, 결과를 반환
+
 from socket import *
 
 port = 3333
@@ -6,8 +9,9 @@ BUFSIZE = 1024
 sock = socket(AF_INET, SOCK_STREAM)
 sock.bind(('', port))
 sock.listen(1)
-conn, (remotehost, remoteport) = sock.accept()
-print("connected by", remotehost, remoteport)
+
+conn, addr = sock.accept()
+print("connected by", addr)
 
 while True:
     try:
@@ -15,12 +19,17 @@ while True:
     except:
         break
     else:
+        # 데이터를 수신할 때 꼭 필요한 예외처리
         if not data:
             break
         res = data.decode()
+
+        # 공백을 기준으로 분리
         res_list = res.split()
+
         num1 = int(res_list[0])
         num2 = int(res_list[2])
+
         if res_list[1] == '+':
             cal_res = num1 + num2
         elif res_list[1] == '-':
@@ -31,6 +40,7 @@ while True:
             cal_res = num1 / num2
 
     try:
+        # 문자열로 변환 후 전송
         conn.send(str(cal_res).encode())
     except:
         conn.send(b'ERROR')
