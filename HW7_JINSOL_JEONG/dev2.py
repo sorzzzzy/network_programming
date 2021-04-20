6af47e54865a856891cd8d6cb6dff5a859f08b19
Jinsol Jeong <jinsol0330@naver.com>
2021-04-21 00:56:43 +0900

    HW7_add comment

--- HW7_JINSOL_JEONG/dev2.py
+++ HW7_JINSOL_JEONG/dev2.py
@@ -1,3 +1,9 @@
+# 2개의 IoT 디바이스로부터 데이터 수집하기
+
+# 디바이스 2: 심박수, 걸음수, 소모칼로리를 측정 제공
+# 사용자로부터 ‘Request’ 메시지를 수신하면 심박수, 걸음수, 소모 칼로리를 전송
+# 사용자로부터 ‘quit’ 메시지를 수신하면 종료
+
 from socket import *
 import random
 
@@ -13,10 +19,14 @@ while True:
     if not msg:
         conn.close()
         continue
+
+    # 사용자로부터 ‘quit’ 메시지를 수신하면 종료
     elif msg == b'quit':
         print('client: ', addr, msg.decode())
         conn.close()
         continue
+
+    # 사용자로부터 ‘Request’ 메시지를 수신하면 심박수, 걸음수, 소모 칼로리를 전송
     elif msg == b'Request':
         print('client: ', addr, msg.decode())
 
--- HW7_JINSOL_JEONG/user.py
+++ HW7_JINSOL_JEONG/user.py
@@ -1,3 +1,9 @@
+# 2개의 IoT 디바이스로부터 데이터 수집하기
+
+# 2개의 IoT 디바이스와 TCP 연결
+# 수집한 데이터는 시간정보를 추가하여 파일에 저장 (파일이름: data.txt)
+
+
 from socket import *
 import time
 
@@ -15,7 +21,9 @@ while True:
     device2_socket = socket(AF_INET, SOCK_STREAM)
     device2_socket.connect(('localhost', 9999))
     
+    # 사용자가 ‘1’을 입력하면 ‘디바이스 1’로 ‘Request’ 메시지를 전송하여 데이터 수집
     if user_input == '1':
+
         device1_socket.send(b'Request')
         msg = device1_socket.recv(BUF_SIZE)
 
@@ -23,10 +31,13 @@ while True:
         now = time.strftime('%c', time.localtime(time.time()))
         string = now + f': Device1: Temp={temp}, Humid={humid}, Illum={illum}\n'
         print(string)
+
+        # 파일 쓰기
         f = open('data.txt', 'a')
         f.write(string)
         f.close()
 
+    # 사용자가 ‘2’를 입력하면 ‘디바이스 2’로 ‘Request’ 메시지를 전송하여 데이터 수집
     elif user_input == '2':
         device2_socket.send(b'Request')
         msg = device2_socket.recv(BUF_SIZE)
@@ -35,10 +46,13 @@ while True:
         now = time.strftime('%c', time.localtime(time.time()))
         string = now + f': Device2: Heartbeat={heartbeat}, Steps={steps}, Cal={cal}\n'
         print(string)
+
+        # 파일 쓰기
         f = open('data.txt', 'a')
         f.write(string)
         f.close()
 
+    # 사용자가 ‘quit’을 입력하면 ‘디바이스 1’, ‘디바이스 2’로 ‘quit’ 메시지를 전송 후 종료
     elif user_input == 'quit':
         device1_socket.send(b'quit')
         device2_socket.send(b'quit')
