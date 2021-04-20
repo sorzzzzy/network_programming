9a727b1f0fffd82ab4a12d787a8cde58e36fd182
Jinsol Jeong <jinsol0330@naver.com>
2021-04-20 21:52:35 +0900

    HW2_add comment

--- HW2_JINSOL_JEONG/hw2_2.py
+++ HW2_JINSOL_JEONG/hw2_2.py
@@ -1,11 +1,23 @@
-def cal_gcd(num1,num2):
-    while num2 > 0:
-        num1, num2 = num2, num1 % num2
-    return num1
+# 두 수의 최대공약수는 두 수를 나누어 떨어지는 가장 큰 수이다. 
+# 예를 들어 (16, 24)의 최대 공약수는 8이다. 
+# 두 수를 입력 받아 다음 알고리즘에 의해 최대 공약수를 구하는 프로그램을 작성하라.
 
-num1 = int(input("첫번째 숫자를 입력하세요 : "))
-num2 = int(input("두번째 숫자를 입력하세요 : "))
+num = input("두 수를 입력해주세요. ex)16 24 : ")
+num = num.split()
 
-result = cal_gcd(num1,num2)
+num1 = int(max(num))
+num2 = int(min(num))
 
-print(result)
+# 작은 수가 0이 될 때 까지 반복
+while num2 != 0:
+    # 큰 수를 작은 수로 나눈 나머지(=res)를 구함
+    res = num1 % num2
+
+    # 큰 수를 작은 수로 대체
+    num1 = num2
+
+    # 작은 수를 나머지(=res)로 대체
+    num2 = res
+
+# 과정을 반복한 뒤 마지막 큰 수가 최대 공약수
+print("Result : ", num1)
--- HW2_JINSOL_JEONG/hw2_4.py
+++ HW2_JINSOL_JEONG/hw2_4.py
@@ -1,15 +1,28 @@
+# 숫자를 문자열로 변화하는 방법은 str(num)을 이용한다. 
+#   --> str(12)→'12'가 된다. 
+# 반대로 문자열을 숫자로 변환하려면 int(string)을 이용한다. 
+#   --> int('12') → 12가 된다. 
+# 이를 이용하여 1부터 1000까지의 숫자의 각 자리수의 합을 모두 구하라. 
+#   --> 예를 들어 234→2+3+4=9가 된다.
+
+# 빈 리스트 생성
 num = []
 sum = []
+
 def num_list(num):
+    # 1~1000 까지 리스트 생성(문자열로)
     for i in range(1,1001):
         num.append(str(i))
     return num
 
 def cal_sum(num, sum):
+    # num 리스트 안에 있는 1~1000 사이의 문자를 하나하나 가져옴
     for s in num:
         res = 0
+        # 숫자의 각 자리수를 정수형으로 변환해 다 더함
         for w in s:
             res += int(w)
+        # sum 리스트에 추가함
         sum.append(res)
     return sum
 
