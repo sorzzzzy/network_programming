caf2f692638f22da38d2f36c603841d345e269b4
Jinsol Jeong <jinsol0330@naver.com>
2021-04-21 00:20:15 +0900

    HW4_add comment

--- HW4_JINSOL_JEONG/hw4_client.py
+++ HW4_JINSOL_JEONG/hw4_client.py
@@ -1,10 +1,22 @@
-import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 문자열(이름), 정수(학번) 전송하기
+# 본인의 이름(예:'JINSOL JEONG')을 문자열로 전송하기
+# 본인의 학번을 수신한 후 정수로 변환하여 출력
+
+from socket import *
+
+sock = socket(AF_INET, SOCK_STREAM)
 addr = ('localhost', 9000)
 sock.connect(addr)
+
+# 'Hello + 클라이언트 주소' 수신 후 출력
 msg = sock.recv(1024)
 print(msg.decode())
+
+# 본인 이름을 문자열로 전송
 sock.send(b'JINSOL JEONG')
+
+# 학번 수신 후 출력
 num_b = sock.recv(1024)
 print(num_b.decode())
+
 sock.close()
--- HW4_JINSOL_JEONG/hw4_server.py
+++ HW4_JINSOL_JEONG/hw4_server.py
@@ -1,14 +1,25 @@
-import socket
+# 문자열(이름), 정수(학번) 전송하기
+# 학생의 이름을 수신한 후 출력
+# 학생의 학번(정수형 변수)을 전송하기
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+from socket import *
+
+s = socket(AF_INET, SOCK_STREAM)
 s.bind(('', 9000))
 s.listen(2)
 
 while True:
     client, addr = s.accept()
+
+    # 클라이언트에게 연결 확인 메시지 보내기
     print('Connection from', addr)
     client.send(b'Hello ' + addr[0].encode())
+
+    # 클라이언트로부터 이름 수신 후 출력
     name = client.recv(1024)
     print(name.decode())
+
+    # 클라이언트에게 학번 보내기
     client.send(b'20171534')
+
     client.close()
